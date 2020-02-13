5e40cb8e19fb5aa2607d8e3c55ec0c9ed79d3d45
Mathias Buus <mathiasbuus@gmail.com>
2020-02-13 14:56:06 +0100

    tweak a bit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const os = require('os')
-const equal = require('fast-deep-equal')
 const { EventEmitter } = require('events')
 
 const INTERVAL = 1000 * 10
@@ -10,38 +9,54 @@ class ConnectivityMonitor extends EventEmitter {
 
     this._interval = opts.interval || INTERVAL
     this._lastPoll = 0
-    this._lastState = null
+    this._lastState = []
 
     this._timer = setInterval(() => {
-      var currentTime = Date.now()
-      var currentState = this._filterInterfaces(os.networkInterfaces())
+      const currentTime = Date.now()
+      const currentState = getCidrs()
 
       const isWakingUp = (currentTime - this._lastPoll) > INTERVAL * 2
-      const isDifferentState = !equal(currentState, this._lastState)
-      if (isWakingUp || isDifferentState) this.emit('change')
+      if (isWakingUp) {
+        this._lastPoll = currentTime
+        this._lastState = []
+        return
+      }
+
+      const isDifferentState = !equalArray(currentState, this._lastState)
+      if (isDifferentState) this.emit('change')
 
       this._lastPoll = currentTime
       this._lastState = currentState
     }, this._interval)
     if (opts.noUnref !== true) this._timer.unref()
   }
-
-  _filterInterfaces (networkInterfaces) {
-    const filtered = {}
-    for (const name of Object.keys(networkInterfaces)) {
-      const iface = networkInterfaces[name]
-      if (iface.internal) continue
-      filtered[name] = iface
-    }
-    return filtered
-  }
-
+  
   destroy () {
     if (this._timer) clearInterval(this._timer)
     this._timer = null
   }
 }
+
 module.exports = ConnectivityMonitor
+
+function getCidrs () {
+  const all = []
+  const faces = os.networkInterfaces()
+  for (const name of Object.keys(faces)) {
+    for (const i of faces[name]) {
+      if (!i.internal) all.push(i.cidr)
+    }
+  }
+  return all
+}
+
+function equalArray (a, b) {
+  if (a.length !== b.length) return false
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false
+  }
+  return true
+}
 
 if (require.main === module) {
   const monitor = new ConnectivityMonitor({ interval: 1000, noUnref: true })
